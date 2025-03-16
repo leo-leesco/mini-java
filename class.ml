d052c7ca85b1508d0369b676d5c243bedbe71b98
Léo LEESCO <leo.leesco@gmail.com>
2025-03-16 15:09:02 +0100

    move assert to class.assert

--- class.ml
+++ class.ml
@@ -42,3 +42,95 @@ let rec class_from_string (p : Ast.pfile)
 let classes (classes : (string, string option) Hashtbl.t) (p : Ast.pfile) =
   List.of_seq
     (Seq.map (class_from_string p classes) (Hashtbl.to_seq_keys classes))
+
+(** Assert that the classes are well formed @raise Error(location, comment) *)
+module Assert = struct
+  let dummy_loc = (Lexing.dummy_pos, Lexing.dummy_pos)
+
+  exception Error of Ast.location * string
+
+  (* use the following function to signal typing errors, e.g.
+      error ~loc "unbound variable %s" id
+*)
+  let error ?(loc = dummy_loc) f =
+    Format.kasprintf (fun s -> raise (Error (loc, s))) ("@[" ^^ f ^^ "@]")
+
+  (**classes are uniquely defined*)
+  let unique (p : Ast.pfile) =
+    let class_count = Hashtbl.create (List.length p) in
+    List.iter
+      (fun ({ loc; id }, _, _) ->
+        match Hashtbl.find_opt class_count id with
+        | None -> Hashtbl.add class_count id ()
+        | Some () -> error ~loc "class %s already defined" id)
+      p
+
+  (**Classes do not inherit from String*)
+  let no_string_inheritance (p : Ast.pfile) =
+    match p with
+    | [] -> ()
+    | (this, extends, _) :: others -> (
+        match extends with
+        | Some extends when extends.id = "String" ->
+            let loc = this.loc in
+            error ~loc "%s inherits from String" this.id
+        | _ -> ())
+
+  (**classes inherit from a class that is defined elsewhere*)
+  let valid_extends (p : Ast.pfile)
+      (classes : (string, string option) Hashtbl.t) =
+    Hashtbl.iter
+      (fun this extends ->
+        match extends with
+        | Some cls ->
+            if not (Hashtbl.mem classes cls) then
+              let { loc }, _, _ = pclass_from_string p this in
+              error ~loc "Invalid extends : superclass %s is never defined" this
+        | _ -> ())
+      classes
+
+  let rec is_cycle (classes : (string, string option) Hashtbl.t)
+      (in_cycle : (string, bool) Hashtbl.t) (turtle : string) (hare : string) =
+    match Hashtbl.find classes hare with
+    | None ->
+        Hashtbl.add in_cycle hare false;
+        false
+    | Some parent_hare -> (
+        match Hashtbl.find classes parent_hare with
+        | None ->
+            Hashtbl.add in_cycle hare false;
+            false
+        | Some hare -> (
+            let turtle = Option.get (Hashtbl.find classes turtle) in
+            if turtle = hare then (
+              Hashtbl.add in_cycle turtle true;
+              Hashtbl.add in_cycle hare true;
+              true)
+            else
+              try
+                let already_visited =
+                  Hashtbl.find in_cycle turtle || Hashtbl.find in_cycle hare
+                in
+                if already_visited then true
+                else
+                  let new_in_cycle = is_cycle classes in_cycle turtle hare in
+                  Hashtbl.add in_cycle turtle new_in_cycle;
+                  Hashtbl.add in_cycle hare new_in_cycle;
+                  new_in_cycle
+              with Not_found ->
+                let new_in_cycle = is_cycle classes in_cycle turtle hare in
+                Hashtbl.add in_cycle turtle new_in_cycle;
+                Hashtbl.add in_cycle hare new_in_cycle;
+                new_in_cycle))
+
+  (**there is no circular inheritance*)
+  let acyclical (p : Ast.pfile) (classes : (string, string option) Hashtbl.t) =
+    let in_cycle = Hashtbl.create (Hashtbl.length classes) in
+
+    Hashtbl.iter
+      (fun this _ ->
+        if is_cycle classes in_cycle this this then
+          let { loc }, _, _ = pclass_from_string p this in
+          error ~loc "%s is in cyclic dependency" this)
+      classes
+end
--- typing.ml
+++ typing.ml
@@ -1,19 +1,18 @@
 open Ast
 open Class
-open Assert
 
 let debug = ref false
 
-let file ?debug:(b = false) (p : Ast.pfile) : Ast.tfile =
-  debug := b;
-
-  Assert.unique p;
-  Assert.no_string_inheritance p;
+let file ?(debug = !debug) (p : Ast.pfile) : Ast.tfile =
+  Class.Assert.unique p;
+  Class.Assert.no_string_inheritance p;
 
   let quick_look = Class.quick_look p in
 
-  Assert.valid_extends p quick_look;
-  Assert.acyclical p quick_look;
+  Class.Assert.valid_extends p quick_look;
+  Class.Assert.acyclical p quick_look;
 
   let classes = Class.classes quick_look p in
+  if debug then print_endline "Class tree built";
+
   failwith "TODO"
