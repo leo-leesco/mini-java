9a5b9ad8191702f4bc91708c44823d7579b58446
Léo LEESCO <leo.leesco@gmail.com>
2025-03-16 12:20:26 +0100

    assert no class inherits from String

--- typing.ml
+++ typing.ml
@@ -29,114 +29,114 @@ let file ?debug:(b = false) (p : Ast.pfile) : Ast.tfile =
   in
   assert_unique p;
 
-  try
-    let { loc; id }, _, _ =
-      List.find
-        (fun (_, ext, _) ->
-          match ext with Some ext -> ext.id = "String" | None -> false)
-        p
+  let assert_no_string_inheritance (p:Ast.pfile) = 
+    match p with
+    | [] -> ()
+    | (this,extends,_) :: others -> (match extends with
+        | Some extends when extends.id = "String"-> 
+          let loc = this .loc in
+          error ~loc "%s inherits from String" this.id
+        |_ -> ()
+      )
+  in
+  assert_no_string_inheritance p;
+
+  let classes =
+    let tree : (string, string option) Hashtbl.t =
+      Hashtbl.create (List.length p)
     in
-    error ~loc "%s inherits from String" id
-  with Not_found ->
-    ();
+    List.iter
+      (fun (this, extends, _) ->
+         Hashtbl.add tree this.id
+           (match extends with None -> None | Some cls -> Some cls.id))
+      p;
+    tree
+  in
 
-    let classes =
-      let tree : (string, string option) Hashtbl.t =
-        Hashtbl.create (List.length p)
-      in
-      List.iter
-        (fun (this, extends, _) ->
-          Hashtbl.add tree this.id
-            (match extends with None -> None | Some cls -> Some cls.id))
-        p;
-      tree
-    in
+  let assert_valid_extends (classes : (string, string option) Hashtbl.t) =
+    Hashtbl.iter
+      (fun this extends ->
+         match extends with
+         | Some cls ->
+           if not (Hashtbl.mem classes cls) then
+             let { loc }, _, _ = pclass_from_string this in
+             error ~loc "Invalid extends : superclass %s is never defined"
+               this
+         | _ -> ())
+      classes
+  in
+  assert_valid_extends classes;
 
-    let assert_valid_extends (classes : (string, string option) Hashtbl.t) =
-      Hashtbl.iter
-        (fun this extends ->
-          match extends with
-          | Some cls ->
-              if not (Hashtbl.mem classes cls) then
-                let { loc }, _, _ = pclass_from_string this in
-                error ~loc "Invalid extends : superclass %s is never defined"
-                  this
-          | _ -> ())
-        classes
-    in
-    assert_valid_extends classes;
+  let assert_acyclical (classes : (string, string option) Hashtbl.t) =
+    let in_cycle = Hashtbl.create (Hashtbl.length classes) in
 
-    let assert_acyclical (classes : (string, string option) Hashtbl.t) =
-      let in_cycle = Hashtbl.create (Hashtbl.length classes) in
-      (* Hashtbl.iter (fun k _ -> Hashtbl.add in_cycle k false) classes; *)
-
-      let rec is_cycle (turtle : string) (hare : string) =
-        match Hashtbl.find classes hare with
-        | None ->
+    let rec is_cycle (turtle : string) (hare : string) =
+      match Hashtbl.find classes hare with
+      | None ->
+        Hashtbl.add in_cycle hare false;
+        false
+      | Some parent_hare -> (
+          match Hashtbl.find classes parent_hare with
+          | None ->
             Hashtbl.add in_cycle hare false;
             false
-        | Some parent_hare -> (
-            match Hashtbl.find classes parent_hare with
-            | None ->
-                Hashtbl.add in_cycle hare false;
-                false
-            | Some hare -> (
-                let turtle = Option.get (Hashtbl.find classes turtle) in
-                if turtle = hare then (
-                  Hashtbl.add in_cycle turtle true;
-                  Hashtbl.add in_cycle hare true;
-                  true)
-                else
-                  try
-                    let already_visited =
-                      Hashtbl.find in_cycle turtle || Hashtbl.find in_cycle hare
-                    in
-                    if already_visited then true
-                    else
-                      let new_in_cycle = is_cycle turtle hare in
-                      Hashtbl.add in_cycle turtle new_in_cycle;
-                      Hashtbl.add in_cycle hare new_in_cycle;
-                      new_in_cycle
-                  with Not_found ->
+          | Some hare -> (
+              let turtle = Option.get (Hashtbl.find classes turtle) in
+              if turtle = hare then (
+                Hashtbl.add in_cycle turtle true;
+                Hashtbl.add in_cycle hare true;
+                true)
+              else
+                try
+                  let already_visited =
+                    Hashtbl.find in_cycle turtle || Hashtbl.find in_cycle hare
+                  in
+                  if already_visited then true
+                  else
                     let new_in_cycle = is_cycle turtle hare in
                     Hashtbl.add in_cycle turtle new_in_cycle;
                     Hashtbl.add in_cycle hare new_in_cycle;
-                    new_in_cycle))
-      in
-
-      Hashtbl.iter
-        (fun this _ ->
-          if is_cycle this this then
-            let { loc }, _, _ = pclass_from_string this in
-            error ~loc "%s is in cyclic dependency" this)
-        classes
-    in
-    assert_acyclical classes;
-
-    (** @returns (number of methods, number of attributes)*)
-    let count_methods_attributes (decl : pdecl list) =
-      List.fold_left
-        (fun (m, a) declaration ->
-          match declaration with
-          | PDattribute _ -> (m, a + 1)
-          | PDmethod _ -> (m + 1, a)
-          | _ -> (m, a))
-        (0, 0) decl
+                    new_in_cycle
+                with Not_found ->
+                  let new_in_cycle = is_cycle turtle hare in
+                  Hashtbl.add in_cycle turtle new_in_cycle;
+                  Hashtbl.add in_cycle hare new_in_cycle;
+                  new_in_cycle))
     in
 
-    let rec class_from_string (cls : string) : class_ =
-      let (this,parent,declarations) = pclass_from_string cls in
-      let (m,a)=count_methods_attributes declarations in
-          {
-            class_name = cls;
-            class_extends = (match (Hashtbl.find classes cls)  with
+    Hashtbl.iter
+      (fun this _ ->
+         if is_cycle this this then
+           let { loc }, _, _ = pclass_from_string this in
+           error ~loc "%s is in cyclic dependency" this)
+      classes
+  in
+  assert_acyclical classes;
+
+  (** @returns (number of methods, number of attributes)*)
+  let count_methods_attributes (decl : pdecl list) =
+    List.fold_left
+      (fun (m, a) declaration ->
+         match declaration with
+         | PDattribute _ -> (m, a + 1)
+         | PDmethod _ -> (m + 1, a)
+         | _ -> (m, a))
+      (0, 0) decl
+  in
+
+  let rec class_from_string (cls : string) : class_ =
+    let (this,parent,declarations) = pclass_from_string cls in
+    let (m,a)=count_methods_attributes declarations in
+    {
+      class_name = cls;
+      class_extends = (match (Hashtbl.find classes cls)  with
           | None -> None
           | Some parent -> Some (class_from_string parent));
-          class_methods = Hashtbl.create m;
-          class_attributes = Hashtbl.create a;
-          }
-    in
+      class_methods = Hashtbl.create m;
+      class_attributes = Hashtbl.create a;
+    }
+  in
 
-let classes =  List.of_seq(Seq.map class_from_string(Hashtbl.to_seq_keys classes)) in
+  let classes =  List.of_seq (Seq.map class_from_string(Hashtbl.to_seq_keys classes)) in
 
-    failwith "TODO"
+  failwith "TODO"
